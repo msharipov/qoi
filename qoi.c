f54491b09ec408c73429d146fb3316eb2bbb3074
msharipov <msharipovr@gmail.com>
2023-09-29 01:12:24 -0400

    implemented CLI arguments handling

--- qoi.c
+++ qoi.c
@@ -1,12 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_help() {
+    printf("Usage: qoi [options] FILE \n\n");
+    printf("-f                  Display supported formats.\n");
+    printf("-h                  Display this help.\n");
+}
+
+void list_formats() {
+    printf("Supported formats:\n");
+    printf(".QOI \n");
+    printf(".PPM \n");
+}
+
+void complain_args() {
+    printf("Error! Invalid number of arguments\n");
+    print_help();
+}
+
+void complain_option(char * opt) {
+    printf("Error! Invalid option: %s\n", opt);
+    print_help();
+}
+
+void complain_no_outfile() {
+    printf("Error! Missing filename after '-o'\n");
+}
+
+void complain_no_infile() {
+    printf("Error! Input file was not specified\n");
+}
+
+/* Error codes:
+    1 - Invalid user input
+    2 - Help/Formats screen requested
+*/
 int main(int argc, char * argv[]){
     if (argc < 2) {
-        printf("Invalid number of arguments!\n");
-        printf("USAGE: qoi FILE1 [FILE2] ...\n");
-        printf("Only .ppm and .qoi formats supported.");
+        complain_args();  
+        return 1;      
     }
 
-    for (int file_number = 1; file_number <= argc; file_number++) {
-        const char * FILENAME = argv[file_number];
+    char * infile, * outfile;
+
+    // Handle arguments
+    for (int i = 1; i < argc; i++) {
+    
+        if (argv[i][0] == '-') {
+
+            // When there is no option after '-'
+            if (strlen(argv[i]) == 1) {
+                complain_option(argv[i]);
+                return 1;
+            }
+
+            switch (argv[i][1]) {
+            case 'h':
+                print_help();
+                return 2;
+
+            case 'f':
+                list_formats();
+                return 2;
+
+            case 'o':
+                if (i + 1 < argc && argv[i + 1][0] != '-') {
+                    outfile = argv[i + 1];
+                    i++;
+                } else {
+                    complain_no_outfile();
+                    return 1;
+                }
+                break;
+
+            default:
+                complain_option(argv[i]);
+                return 1;
+            }
+
+        } else {
+
+            infile = argv[i];
+        } 
     }
+
+    if (!infile) {
+        complain_no_infile();
+        return 1;
+    }
+
+    
+
     return 0;
 }
